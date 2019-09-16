2140c672e6b0e7dc6a70bce60042542c79065a9d
Joey Leone <joeyleone18@hotmail.com>
2019-09-16 18:45:08 -0400

    Add project files.

---
+++ Lab1-200402967-F2019/Controllers/NumberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1_200402967_F2019.Controllers
+{
+    public class NumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Addition(int x, int y)
+        {
+            return Ok("Result = " + (x + y).ToString());
+        }
+
+        public IActionResult Multiplication(int x, int y)
+        {
+            return Ok("Result = " + (x * y).ToString());
+        }
+    }
+}
---
+++ Lab1-200402967-F2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1_200402967_F2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
